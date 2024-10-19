fef92021b41eb5ddf9846219fa1124fe0e5f34e8
kalan61 <did890722@naver.com>
2024-10-19 14:48:55 +0900

    BoardSelect 추가

--- src/dto/BoardDTO.java
+++ src/dto/BoardDTO.java
@@ -2,8 +2,8 @@ package dto;
 
 import java.util.Date;
 
-public class BoardDTO {
-	
+public class BoardDTO extends PageDTO {
+
 	// 게시판 아이디
 	private String boardId;
 	// 게시판 제목
@@ -22,60 +22,77 @@ public class BoardDTO {
 	private Date boardLupDt;
 	// 게시판 작성자
 	private String userId;
-	
+
 	public String getBoardId() {
 		return boardId;
 	}
+
 	public void setBoardId(String boardId) {
 		this.boardId = boardId;
 	}
+
 	public String getBoardTitle() {
 		return boardTitle;
 	}
+
 	public void setBoardTitle(String boardTitle) {
 		this.boardTitle = boardTitle;
 	}
+
 	public String getBoardContent() {
 		return boardContent;
 	}
+
 	public void setBoardContent(String boardContent) {
 		this.boardContent = boardContent;
 	}
+
 	public String getBoardWordDate() {
 		return boardWordDate;
 	}
+
 	public void setBoardWordDate(String boardWordDate) {
 		this.boardWordDate = boardWordDate;
 	}
+
 	public String getBoardWordTime() {
 		return boardWordTime;
 	}
+
 	public void setBoardWordTime(String boardWordTime) {
 		this.boardWordTime = boardWordTime;
 	}
+
 	public String getBoardFilePath() {
 		return boardFilePath;
 	}
+
 	public void setBoardFilePath(String boardFilePath) {
 		this.boardFilePath = boardFilePath;
 	}
+
 	public Date getBoardRegDt() {
 		return boardRegDt;
 	}
+
 	public void setBoardRegDt(Date boardRegDt) {
 		this.boardRegDt = boardRegDt;
 	}
+
 	public Date getBoardLupDt() {
 		return boardLupDt;
 	}
+
 	public void setBoardLupDt(Date boardLupDt) {
 		this.boardLupDt = boardLupDt;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
-	
+
 }
--- src/service/BoardService.java
+++ src/service/BoardService.java
@@ -4,5 +4,6 @@ import dto.BoardDTO;
 
 public interface BoardService {
 
+	// 게시판 등록
 	public int insertBoard(BoardDTO dto);
 }
