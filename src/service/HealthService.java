b714f8b65bb3c9d2b6a20f6d8f20928bb443602a
kalan61 <did890722@naver.com>
2024-10-21 18:49:02 +0900

    헬스케어 DB연결

--- src/service/HealthService.java
+++ src/service/HealthService.java
@@ -4,7 +4,16 @@ import dto.HealthDTO;
 
 public interface HealthService {
 
+	// 헬스케어 등록
 	public int insertHealth(HealthDTO dto);
+
+	// 헬스케어 월통계 조회
+	public HealthDTO selectHealthSt(HealthDTO dto);
+
+	// 헬스케어 조회
+	public HealthDTO selectHealth(HealthDTO dto);
+	
+	
 	
 	
 }
